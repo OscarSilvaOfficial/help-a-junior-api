98c77f32c350096309de5cecee8f7f28867e6f7d
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-01-19 18:16:30 -0300

    fazendo funcionar na vercel

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
-from views.hello_world import hello_world  
-from utils.format import server_log
-from config import PORT, ADDR
+from api.views.hello_world import hello_world  
+from api.utils.format import server_log
+from api.config import PORT, ADDR
 
 
 if __name__ == '__main__':
